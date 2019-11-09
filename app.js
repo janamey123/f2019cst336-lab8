570f81482abe33f2e67d78716f415618df138b4d
Jana Meyer <janmeyer@csumb.edu>
2019-11-08 18:14:52 -0800

    lab 8

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
 
-let question1 = false;
-
 // you can optionally use handlebars
 app.set("view engine", "ejs");
-
 app.use(express.static("public")); //access images, css, js
 
 // enable use of json
@@ -16,39 +13,8 @@ app.use(express.urlencoded({extended: false}));
 app.get("/", function(req, res){
 
     res.render("index");
-
 } );
 
-// Step 4. Process the quiz and return the original along with
-// the answer related information. (Though, probably don't need original
-// because the page was never left and still has the answers.)
-app.post("/", function(req, res){
-
-    var q1 = req.questions.question1;
-    //req.questions["1"];
-
-    gradeQuiz(q1);
-
-    res.json({
-        answers: [
-            {
-                question: 1,
-                correct: true,
-            }
-        ],
-        original: req.body
-    })
-
-} );
-
-function gradeQuiz(q1) {
-    //Question 1
-    if (q1Response == "sacramento") {
-        question1 = true;
-    } else {
-        question1 = false;
-    }
-} // gradeQuiz()
 
 
 // running server
